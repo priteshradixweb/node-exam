9e6b3c199d29ee087002c57277a71c52f74ce396
Pritesh Yadav <pritesh.yadav@radixweb.com>
2026-04-25 10:49:05 +0530

    initial commit project setup

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.APP_PORT;
+
+
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
